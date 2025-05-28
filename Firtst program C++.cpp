f66a523665eec5f8a946eb70bf519b9bd710cc48
Chokosdev <georafkostas@gmail.com>
2025-05-28 15:00:18 +0300

    Add files via upload

---
+++ Firtst program C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	//display the first cout
+	cout << "this is my first program" << endl;
+	cout << "ITS WORKING" << endl;
+
+	return 0;// Program ends successfully
+	
+	
+}
